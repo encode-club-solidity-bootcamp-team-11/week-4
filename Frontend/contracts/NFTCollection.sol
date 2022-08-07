f77385cd23f15f6bc88d348d0c65552aeb195630
Sereyboth Chamroeun <ch.sereyboth@gmail.com>
2022-08-07 20:27:54 +0700

    deploy to Goerli

--- Frontend/contracts/NFTCollection.sol
+++ Frontend/contracts/NFTCollection.sol
@@ -1,10 +1,10 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "../node_modules/hardhat/console.sol";
-import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFTCollection is ERC721URIStorage {
 
